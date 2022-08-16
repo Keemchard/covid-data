58a47766579a41554bf40702177b11b0ee91969e
Keemchard <keemchardtamio@gmail.com>
2022-08-16 21:15:19 +0800

    add/display specific more covid data upon clicking see more btn

--- src/components/CovidDataList.tsx
+++ src/components/CovidDataList.tsx
@@ -1,46 +1,91 @@
 import React, { useEffect, useState } from "react";
 //styles
 import "./CovidDataList.component.css";
+import CovidDataSpecific from "./CovidDataSpecific";
 
 const covidDataList = () => {
   const [covidData, setCovidData] = useState([]);
+  const [covidMoreDataUrl, setCovidMoreDataUrl] = useState();
+  const [covidCountryName, setCovidCountryName] = useState<string>("");
+
   const apiUrl: string =
     "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true";
 
-  const anotherUrl: string =
-    "https://api.apify.com/v2/datasets/jaycEQiGMlb7aOmtI/items?format=json&clean=1";
   const getCovidData = async () => {
     const res = await fetch(apiUrl);
     const data = await res.json();
+
     setCovidData(data);
+    console.log(data);
   };
 
   useEffect(() => {
     getCovidData();
   }, []);
 
-  return (
-    <table className=" covid-list-con">
-      <tr>
-        <th>Country</th>
-        <th>Infected</th>
-        <th>Tested</th>
-        <th>Recovered</th>
-        <th>Last Update TIME</th>
-      </tr>
+  if (covidCountryName) {
+    return (
+      <>
+        <CovidDataSpecific
+          covidMoreDataUrl={covidMoreDataUrl}
+          covidCountryName={covidCountryName}
+        />
+        <button
+          onClick={() => {
+            setCovidCountryName("");
+          }}
+        >
+          Back
+        </button>
+      </>
+    );
+  }
 
-      {covidData.map((covid: any) => {
-        return (
-          <tr key={Math.random()}>
-            <td>{covid.country}</td>
-            <td>{covid.infected}</td>
-            <td>{covid.tested}</td>
-            <td>{covid.recovered}</td>
-            <td>{covid.lastUpdatedApify}</td>
-          </tr>
-        );
-      })}
-    </table>
+  return (
+    <>
+      <table className="covid-list-con">
+        <tr>
+          <th>
+            <h1>Country</h1>
+          </th>
+          <th>
+            <h1>Infected</h1>
+          </th>
+          <th>
+            <h1>Tested</h1>
+          </th>
+          <th>
+            <h1>Recovered</h1>
+          </th>
+          <th>
+            <h1>Last Update</h1>
+          </th>
+        </tr>
+
+        {covidData.map((covid: any) => {
+          return (
+            <tr key={Math.random()}>
+              <td style={{ display: "flex", flexDirection: "column" }}>
+                {covid.country}
+                <button
+                  onClick={() => {
+                    setCovidMoreDataUrl(covid.moreData);
+                    setCovidCountryName(covid.country);
+                  }}
+                  style={{ marginTop: "10px", padding: "5px" }}
+                >
+                  See More
+                </button>
+              </td>
+              <td>{covid.infected}</td>
+              <td>{covid.tested}</td>
+              <td>{covid.recovered}</td>
+              <td>{covid.lastUpdatedApify}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </>
   );
 };
 
---
+++ src/components/CovidDataSpecific.tsx
@@ -0,0 +1,54 @@
+import { read } from "fs";
+import React, { useEffect, useState } from "react";
+//styles
+import "./CovidDataSpecific.component.css";
+
+const CovidDataSpecific = ({ covidMoreDataUrl, covidCountryName }: any) => {
+  const [covidMoreData, setCovidMoreData] = useState({});
+
+  const getMoreCovidData = async () => {
+    const res = await fetch(covidMoreDataUrl);
+    const data = await res.json();
+    console.log(data);
+    setCovidMoreData(data);
+  };
+
+  useEffect(() => {
+    getMoreCovidData();
+  }, []);
+
+  const {
+    activeCases,
+    critical,
+    deceased,
+    infected,
+    lastUpdateAtApify,
+    readMe,
+    recovered,
+    sourceUrl,
+    tested,
+  }: any = covidMoreData;
+
+  return (
+    <>
+      <div className="more-data-con">
+        <div className="more-data-head">
+          <h1>{covidCountryName}</h1>
+        </div>
+        <div className="more-data-details">
+          <p>{`Active Cases: ${activeCases}`}</p>
+          <p>{`Critical Cases: ${critical}`}</p>
+          <p>{`Deceased: ${deceased}`}</p>
+          <p>{`Infected: ${infected}`}</p>
+          <p>{`Tested: ${tested}`}</p>
+          <p>{`Recovered: ${recovered}`}</p>
+          <p>{`Latest update time: ${lastUpdateAtApify}`}</p>
+          <p>{`Read more: ${readMe}`}</p>
+          <p>{`Source Url: ${sourceUrl}`}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CovidDataSpecific;
